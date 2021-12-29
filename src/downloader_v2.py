dae786456bfabf63a67c811ebdbb5eefa41a7655
shiba2046@work <17551496+shiba2046@users.noreply.github.com>
2021-12-29 18:07:19 +0800

    Now supports multprocessing

---
+++ src/downloader_v2.py
@@ -0,0 +1,167 @@
+import typer
+import json
+import requests
+from bs4 import BeautifulSoup
+from pathlib import Path
+import subprocess
+import ffmpeg
+import multiprocessing as mp
+
+app = typer.Typer()
+
+
+
+class Book(object):
+    def __init__(self, book_data: dict):
+        self.video_data = book_data
+        self.count = book_data['videos']
+        
+        self.desc = book_data['desc']
+        self.video_id = book_data['bvid']
+        self.episodes = [Episode(x, self) for x in book_data['pages']]
+
+
+    
+        self.title = book_data['title']
+        if len(self.title) > 10:
+            self.title = input(f"书籍名称 [{self.title}] : ") or self.title
+        
+        self.author = input(f"作者名称  : ") 
+
+
+class Episode(object):
+    def __init__(self, episode_data: dict, book: object = None):
+        # typer.echo(episode_data)
+        self.raw_data = episode_data
+        
+        self.index = episode_data['page'] 
+        
+        self.title = episode_data['part']
+        self.filename = f"{self.index:02}.{self.title}".replace(' ', '.')
+        
+        
+
+class BilibiliAudiobookDownloader(object):
+    def __init__(self, 
+            url: str, 
+            download_path: str = './download',
+            audio_path: str = './audio'):
+        url_parts = requests.utils.urlparse(url)
+        self.base_url = requests.utils.urlunparse((url_parts.scheme, url_parts.netloc, url_parts.path, "", "", ""))
+    
+        self.data = self.fetch_content_table(self.base_url)
+        self.video_data = self.data['videoData']
+        # Create book
+        self.book = Book(self.video_data)
+
+        self.download_path = Path(download_path) / self.book.title
+        self.download_path.mkdir(parents=True, exist_ok=True)
+        self.audio_path = Path(audio_path) / self.book.title
+        self.audio_path.mkdir(parents=True, exist_ok=True)
+
+
+
+    def fetch_content_table(self, url: str):
+        # Only keep the top most parts
+
+        html = requests.get(url)
+        
+        if html.status_code != 200:
+            typer.secho(f"Failed to fetch the page. Error code {html.status_code}", fg="red")
+            typer.Exit(-1)
+
+        soup = BeautifulSoup(html.text, "html.parser")
+        
+        
+
+        # TODO: IF BILIBILI CHANGES WEBSITE THIS NEEDS TO CHANGE
+        text = soup.find_all('script')[4].text
+        start_pos = text.find('{')
+        end_pos = text.find('};') + 1
+
+        # data = json.loads(text[start_pos:end_pos])
+        # print(data.keys)
+
+        # temp = Path('./temp')
+        # temp.mkdir(parents=True, exist_ok=True)
+        # TODO: FOR DEBUGGING
+        with open(self.download_path / 'debug.html', 'w') as f:
+            f.write(text)
+
+        with open(self.download_path / 'debug.txt', 'w') as f:
+            f.write(text[start_pos:end_pos])
+
+        filename = f'{self.download_path / self.book.title}.json' 
+        with open(filename, 'w', encoding='utf-8') as f:
+            json.dump(self.data, f, ensure_ascii=False, indent=4)        
+        
+        return json.loads(text[start_pos:end_pos])
+
+
+    def download_episode(self, episode: Episode):
+
+        url = f"{self.base_url}?p={episode.index}"
+        download_file = (self.download_path / str(episode.index)).as_posix()
+        audio_file = (self.audio_path / f"{episode.filename}.m4b").as_posix()
+
+        # Download the video
+        typer.echo(f"Downloading {episode.title}")
+        subprocess.run(['you-get', '--no-caption', '-O', download_file, url])
+
+        download_file = next(self.download_path.glob(f"{episode.index}.*")).as_posix()
+        metadata = {
+            'title': episode.title,
+            'track': episode.index,
+            'album': self.book.title,
+            'artist': self.book.author,
+            'album_artist': self.book.author,
+            'description': self.book.desc,
+        }
+        meta = {}
+        for key in metadata.keys():
+            meta[f'metadata:g:{key}'] = f"{key}={metadata[key]}"
+        
+        typer.echo(f"Converting {episode.title}; meta: {meta}")
+        out = (ffmpeg
+            .input(download_file)
+            .output(audio_file, vn=None, acodec='copy', **meta)
+            .overwrite_output()
+            .global_args('-loglevel', 'quiet')
+            .run()
+        )
+
+    
+
+    def download_all_episodes_mp(self):
+        with mp.Pool(mp.cpu_count()) as pool:
+            pool.map(self.download_episode, self.book.episodes)
+
+
+        
+    
+    def download_all_episodes(self):
+        for episode in self.book.episodes:
+            self.download_episode_mp(episode)
+
+@app.command()
+def run(url: str = None, 
+    download_path: str = './download', 
+    audio_path: str = './audiobooks',
+    book_title: str = None,
+    author: str = None
+    ):
+    
+    if url is None:
+        url = input("Bilibili Audio Book URL : ")
+    
+    b = BilibiliAudiobookDownloader(url= url, 
+            download_path= download_path, 
+            audio_path= audio_path
+            )
+    
+    # b.download_all_episodes()
+    b.download_all_episodes_mp()
+
+
+if __name__ == '__main__':
+    app()
